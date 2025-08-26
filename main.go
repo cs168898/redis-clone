638b773d5f57b83072d4d2eb9f984c5f40c6b2aa
Samuel Yew Han Sen <135582318+cs168898@users.noreply.github.com>
2025-08-26 08:04:14 +0000

    Created parsing of input string parser to handle RESP commands

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main(){
+	// declare the input
+	input := "$5\r\nAhmed\r\n"
+
+	reader := bufio.NewReader(strings.NewReader(input))
+
+	// reading the data type ( by reading the first byte in the buffer )
+	b, _ := reader.ReadByte()
+
+	if b != '$'{
+		fmt.Println("Invalid type, expecting bulk strings only which should contain '$'")
+		os.Exit(1)
+	}
+
+	// read and parse the size into an integer
+	size, _ := reader.ReadByte()
+	
+	strSize, _ := strconv.ParseInt(string(size), 10, 64)
+
+	// just remove the \r\n
+	reader.ReadByte()
+	reader.ReadByte()
+
+	name := make([]byte, strSize)
+	reader.Read(name)
+
+	fmt.Println(string(name))
+	// // start a TCP listener for Client to communicate with it
+	// l, err := net.Listen("tcp", ":6379")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// // start receiving requests
+	// conn, err := l.Accept()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// defer conn.Close()
+
+	// // create an infinite loop to receive commands from clients and respond to them
+	// for {
+	// 	buf := make([]byte, 1024)
+
+	// 	// read the messages from clients
+	// 	_, err = conn.Read(buf)
+	// 	if err != nil {
+	// 		if err == io.EOF{
+	// 			break
+	// 		}
+	// 		fmt.Println("error reading from client: ",err.Error())
+	// 		os.Exit(1)
+	// 	}
+
+	// 	// ignore request and send back a PONG
+	// 	conn.Write([]byte("+OK\r\n"))
+	// }
+
+}
