06a1c2de959a868da5684be3d7c77e285212fdca
Samuel Yew Han Sen <135582318+cs168898@users.noreply.github.com>
2025-08-27 09:53:22 +0000

    modified main.go to call NewResp struct and call the Read function then print out the value received

--- main.go
+++ main.go
@@ -1,72 +1,68 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"net"
 )
 
 func main(){
-	// declare the input
-	input := "$5\r\nAhmed\r\n"
+	// // declare the input
+	// input := "$5\r\nAhmed\r\n"
 
-	reader := bufio.NewReader(strings.NewReader(input))
+	// reader := bufio.NewReader(strings.NewReader(input))
 
-	// reading the data type ( by reading the first byte in the buffer )
-	b, _ := reader.ReadByte()
+	// // reading the data type ( by reading the first byte in the buffer )
+	// b, _ := reader.ReadByte()
 
-	if b != '$'{
-		fmt.Println("Invalid type, expecting bulk strings only which should contain '$'")
-		os.Exit(1)
+	// if b != '$'{
+	// 	fmt.Println("Invalid type, expecting bulk strings only which should contain '$'")
+	// 	os.Exit(1)
+	// }
+
+	// // read and parse the size into an integer
+	// size, _ := reader.ReadByte()
+	
+	// strSize, _ := strconv.ParseInt(string(size), 10, 64)
+
+	// // just remove the \r\n
+	// reader.ReadByte()
+	// reader.ReadByte()
+
+	// name := make([]byte, strSize)
+	// reader.Read(name)
+
+	// fmt.Println(string(name))
+	fmt.Println("Listening on port :6379")
+
+	// start a TCP listener for Client to communicate with it
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	// read and parse the size into an integer
-	size, _ := reader.ReadByte()
-	
-	strSize, _ := strconv.ParseInt(string(size), 10, 64)
+	// start receiving requests
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	// just remove the \r\n
-	reader.ReadByte()
-	reader.ReadByte()
+	defer conn.Close()
 
-	name := make([]byte, strSize)
-	reader.Read(name)
+	// create an infinite loop to receive commands from clients and respond to them
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil{
+			fmt.Println(err)
+			return
+		}
 
-	fmt.Println(string(name))
-	// // start a TCP listener for Client to communicate with it
-	// l, err := net.Listen("tcp", ":6379")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+		fmt.Println(value)
 
-	// // start receiving requests
-	// conn, err := l.Accept()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// defer conn.Close()
-
-	// // create an infinite loop to receive commands from clients and respond to them
-	// for {
-	// 	buf := make([]byte, 1024)
-
-	// 	// read the messages from clients
-	// 	_, err = conn.Read(buf)
-	// 	if err != nil {
-	// 		if err == io.EOF{
-	// 			break
-	// 		}
-	// 		fmt.Println("error reading from client: ",err.Error())
-	// 		os.Exit(1)
-	// 	}
-
-	// 	// ignore request and send back a PONG
-	// 	conn.Write([]byte("+OK\r\n"))
-	// }
+		// ignore request and send back a PONG
+		conn.Write([]byte("+OK\r\n"))
+	}
 
 }
